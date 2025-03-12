64c65e9e204b1245533b315e3c7f6a24459bf37b
macgr <heli.issame@gmail.com>
2025-03-12 00:52:28 +0000

    Remove obsolete database files and update assembly informational version

--- obj/Debug/net9.0/Maroccantaginerecipe.AssemblyInfo.cs
+++ obj/Debug/net9.0/Maroccantaginerecipe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maroccantaginerecipe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a7eed3f244fac492528553a4dba1874fbe903b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maroccantaginerecipe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maroccantaginerecipe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
